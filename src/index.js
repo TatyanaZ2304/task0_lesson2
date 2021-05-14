ed7bd6be172923292d534ed7a620bf94b41b4eb7
Tatyana <brillenium@gmail.com>
2021-05-14 11:38:50 +0300

    added new files

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import HelloWorld from './App';
+
+
+ReactDOM.render(
+  <HelloWorld/>,
+  document.getElementById('root')
+);
+
+
